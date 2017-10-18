0f49a7de40a60a7c47bb51ae078a214ef3f46b50
Leandro Machado Pereira <llpereiras@gmail.com>
2017-10-18 16:22:33 -0200

    Ajustando migration de truckers

--- db/migrate/20171004155258_create_truckers.rb
+++ db/migrate/20171004155258_create_truckers.rb
@@ -5,6 +5,7 @@ class CreateTruckers < ActiveRecord::Migration[5.1]
       t.string :phone
       t.integer :type
       t.integer :body_type
+      t.references :origin, :null => true
     end
   end
 end
