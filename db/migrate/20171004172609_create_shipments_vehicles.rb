e62f043be9ffa4a738e826308a846f4997931929
Leandro Machado Pereira <llpereiras@gmail.com>
2017-10-18 16:22:58 -0200

    Retirando migration errada

--- db/migrate/20171004172609_create_shipments_vehicles.rb
+++
@@ -1,8 +0,0 @@
-class CreateShipmentsVehicles < ActiveRecord::Migration[5.1]
-  def change
-    create_table :shipments_vehicles do |t|
-      t.references :shipment, foreign_key: true
-      t.references :vehicle, foreign_key: true
-    end
-  end
-end
