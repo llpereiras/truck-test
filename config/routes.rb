5181a7000b5c8917032b64f98ea826e72d60969d
Leandro Machado Pereira <llpereiras@gmail.com>
2017-10-18 16:22:04 -0200

    Ajustando rotas. Retirando vehicles e adicionando last_location

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     get '/shipments/:id/nearby_truckers', to: 'shipments#nearby_truckers'
+    put '/truckers/:id/last_location', to: 'truckers#last_location'
     resources :shipments
     resources :contacts
     resources :truckers
-    resources :vehicles
     resources :destinations
     resources :origins
   end
