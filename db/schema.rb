5a1772882c17040fb04aefc850ba1b36416deb81
Leandro Machado Pereira <llpereiras@gmail.com>
2017-10-20 17:23:06 -0200

    Atualizando schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004172609) do
+ActiveRecord::Schema.define(version: 20171005172609) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "phone"
+  end
+
+  create_table "contacts_truckers", force: :cascade do |t|
+    t.integer "contact_id"
+    t.integer "trucker_id"
+    t.index ["contact_id"], name: "index_contacts_truckers_on_contact_id"
+    t.index ["trucker_id"], name: "index_contacts_truckers_on_trucker_id"
   end
 
   create_table "destinations", force: :cascade do |t|
@@ -40,23 +47,20 @@ ActiveRecord::Schema.define(version: 20171004172609) do
     t.index ["origin_id"], name: "index_shipments_on_origin_id"
   end
 
-  create_table "shipments_vehicles", force: :cascade do |t|
+  create_table "shipments_truckers", force: :cascade do |t|
     t.integer "shipment_id"
-    t.integer "vehicle_id"
-    t.index ["shipment_id"], name: "index_shipments_vehicles_on_shipment_id"
-    t.index ["vehicle_id"], name: "index_shipments_vehicles_on_vehicle_id"
+    t.integer "trucker_id"
+    t.index ["shipment_id"], name: "index_shipments_truckers_on_shipment_id"
+    t.index ["trucker_id"], name: "index_shipments_truckers_on_trucker_id"
   end
 
   create_table "truckers", force: :cascade do |t|
     t.string "name"
     t.string "phone"
-    t.integer "vehicle_id"
-    t.index ["vehicle_id"], name: "index_truckers_on_vehicle_id"
-  end
-
-  create_table "vehicles", force: :cascade do |t|
     t.integer "type"
     t.integer "body_type"
+    t.integer "origin_id"
+    t.index ["origin_id"], name: "index_truckers_on_origin_id"
   end
 
 end
